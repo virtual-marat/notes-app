61da5798fd7c0c97cd4f561e984d151571c79916
virtual-marat <virtual.marat@gmail.com>
2016-03-27 11:44:43 +0500

    Модель заметки, методы для REST-запросов, файл конфигурации

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {serverPort} from '../etc/config.json'
+import * as db from './utils/DataBaseUtils.js';
+
+db.setUpConnection();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/notes', (req, res) => {
+    db.listNotes().then(data => res.send(data));
+});
+
+app.post('/notes', (req, res) => {
+    db.createNote(req.body).then(data => res.send(data));
+});
+
+app.delete('/notes/:id', (req, res) => {
+    db.deleteNote(req.params.id).then(data => res.send(data));
+});
+
+const server = app.listen(serverPort, () => {
+    console.log(`Server is up and running on port ${serverPort}`)
+});
---
+++ server/utils/DataBaseUtils.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import config from '../../etc/config.json';
+import '../models/Note';
+
+const Note = mongoose.model('Note');
+
+export function setUpConnection() {
+    mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/notes`);
+}
+
+export function listNotes() {
+    return Note.find();
+}
+
+export function createNote(data) {
+    const note = new Note({
+        title     : data.title,
+        text      : data.text,
+        color     : data.color,
+        createdAt : new Date()
+    });
+    return note.save();
+}
+
+export function deleteNote(id) {
+    return Note.findById(id).remove();
+}
