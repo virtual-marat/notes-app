53e80787df7487cb54c49a9b14c22cd57740efd1
virtual-marat <virtual.marat@gmail.com>
2016-03-28 23:12:09 +0500

    Верстка и функционал компонентов: App и NoteEditor

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import NoteEditor from './NoteEditor.jsx';
 import NotesGrid from './NotesGrid.jsx';
 
+import './App.less';
+
 const App = React.createClass({
+    handleNoteAdd(data) {
+        console.log(data);
+    },
     render() {
         return (
             <div className="App">
                 <h2 className="App__header">NotesApp</h2>
-                <NoteEditor />
+                <NoteEditor onNoteAdd="{this.handleNoteAdd}"/>
                 <NotesGrid />
             </div>
         );
--- client/components/NoteEditor.jsx
+++ client/components/NoteEditor.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
 
+import './NoteEditor.less';
+
 const NotesEditor = React.createClass({
+    getInitialState() {
+        return {
+            title : '',
+            text  : '',
+            color : '#ffffff'
+        };
+    },
+    handleTextChange(event) {
+        this.setState({text : event.target.value});
+    },
+    handleTitleChange (event) {
+        this.setState({title : event.target.value});
+    },
+    handleNoteAdd() {
+        const newNote = {
+            title : this.state.title,
+            text  : this.state.text,
+            color : this.state.color
+        };
+
+        this.props.onNoteAdd(newNote);
+        this.setState({text : '', title : '', color : '#ffffff'});
+    },
     render() {
-        return <h1>NotesEditor</h1>;
+        return (
+            <div className="NoteEditor">
+                <input
+                    type="text"
+                    className='NoteEditor__title'
+                    placeholder='Enter title'
+                    value={this.state.title}
+                    onChange={this.handleTitleChange}
+                />
+                <textarea
+                    placeholder='Enter note text'
+                    rows={5}
+                    className='NoteEditor__text'
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <div className="NoteEdtor__footer">
+                    <button
+                        className="NoteEditor__button"
+                        disabled={!this.state.next}
+                        onClick={this.handleNoteAdd}
+                    >
+                        Add
+                    </button>
+                </div>
+            </div>
+        );
     }
 });
 
